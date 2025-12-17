0b58101971af4ccb6792b81c25c1e60b4faf9303
ArthurN <arthurn@theodo.fr>
2025-12-17 12:26:43 +0100

    Initial Commit

---
+++ Pinte ou Matcha/Pinte_ou_MatchaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinte_ou_MatchaApp.swift
+//  Pinte ou Matcha
+//
+//  Created by Arthur Naudy on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Pinte_ou_MatchaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
